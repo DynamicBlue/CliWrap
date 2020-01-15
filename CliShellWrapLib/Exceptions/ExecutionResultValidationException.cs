08011706ec4365f9e17e5559efb11ae381368ae3
huajusnoft <huajunsoft@outook.com>
2020-01-15 18:10:37 +0800

    update

---
+++ CliShellWrapLib/Exceptions/ExecutionResultValidationException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using CliShellWrapLib.Models;
+
+namespace CliShellWrapLib.Exceptions
+{
+    /// <summary>
+    /// Base class for exceptions that are thrown when an instance of <see cref="ExecutionResult"/> fails a certain validation.
+    /// </summary>
+    public abstract class ExecutionResultValidationException : Exception
+    {
+        /// <summary>
+        /// Execution result.
+        /// </summary>
+        public ExecutionResult ExecutionResult { get; }
+
+        /// <summary>
+        /// Initializes an instance of <see cref="ExecutionResultValidationException"/>.
+        /// </summary>
+        protected ExecutionResultValidationException(ExecutionResult executionResult, string? message)
+            : base(message)
+        {
+            ExecutionResult = executionResult;
+        }
+    }
+}
---
+++ CliShellWrapLib/Internal/Signal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CliShellWrapLib.Internal
+{
+    internal class Signal : IDisposable
+    {
+        private readonly SemaphoreSlim _semaphore = new SemaphoreSlim(0);
+
+        public void Release() => _semaphore.Release();
+
+        public void Wait() => _semaphore.Wait();
+
+        public Task WaitAsync() => _semaphore.WaitAsync();
+
+        public void Dispose() => _semaphore.Dispose();
+    }
+}
